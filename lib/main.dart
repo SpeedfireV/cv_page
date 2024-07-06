c3adec791c0e96917b6176247aba56f23619e702
grzeg <grzegorz.niespodziany@gmail.com>
2024-07-06 14:35:25 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: ListView(children: [
+          const Row(
+            children: [
+              Text("Projects"),
+            ],
+          ),
+          // PAGE
+          Column(children: [Text("data")],)
+
+        ],),
+      )
+    );
+  }
+}
