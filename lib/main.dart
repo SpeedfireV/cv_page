bba1bb60c3d753c6e34e908fb977f3421cc1c1e6
Grzegorz Niespodziany <grzegorz.niespodzianek@gmail.com>
2024-07-14 15:08:57 +0200

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class MyApp extends StatelessWidget {
                 height: 10,
               ),
               // PAGE
-              const GitUpdatesPage(),
+              const ProjectsPage(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
